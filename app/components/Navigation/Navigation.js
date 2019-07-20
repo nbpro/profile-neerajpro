7851466b5ba38fa4c24b8cd9af0ab5de5c23c490
gs-nkumar <nkumar@gainsight.com>
2019-07-20 17:36:29 +0530

    Added image to home and download resume section

--- app/components/Navigation/Navigation.js
+++ app/components/Navigation/Navigation.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Logo from "../logo";
+import "./style.scss";
 
 export class Navigation extends React.Component {
   constructor(props) {
     super(props);
     this.state = { addClass: false };
   }
+
   toggle() {
     this.setState({ addClass: !this.state.addClass });
   }
+
   render() {
     return (
       <section>
@@ -16,17 +19,23 @@ export class Navigation extends React.Component {
           <nav className="navbar" role="navigation" aria-label="main navigation">
             <div className="navbar-brand">
               <Logo />
-              <a role="button" className={`navbar-burger ${this.state.addClass ? "is-active" : ''} `} onClick={this.toggle.bind(this)} aria-label="menu" aria-expanded="false" data-target="#navMenu">
+              <a role="button" className={`navbar-burger ${this.state.addClass ? "is-active" : ""} `} onClick={this.toggle.bind(this)} aria-label="menu" aria-expanded="false" data-target="#navMenu">
                 <span aria-hidden="true"></span>
                 <span aria-hidden="true"></span>
                 <span aria-hidden="true"></span>
               </a>
             </div>
-            <div className={`navbar-menu ${this.state.addClass ? "is-active" : ''}`} id="navMenu">
+            <div className={`navbar-module navbar-menu ${this.state.addClass ? "is-active" : ""}`} id="navMenu">
               <div className="navbar-end">
-                <a className="navbar-item">About</a>
-                <a className="navbar-item">Projects</a>
-                <a className="navbar-item">Contact</a>
+                <span className="navbar-item">
+                  <a className="link">About</a>
+                </span>
+                <span className="navbar-item">
+                  <a className="link">Projects</a>
+                </span>
+                <span className="navbar-item">
+                  <a className="link">Contact</a>
+                </span>
               </div>
             </div>
           </nav>
